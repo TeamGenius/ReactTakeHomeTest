0637965a9eacfe63bbbf78378d03d4cc32de61f5
Stephen Kelzer <stephen@stephenkelzer.com>
2021-01-27 16:49:01 -0600

    bringing in challenge boilerplate

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import players from './_players.json';
+
+const App = () => {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>hello</td>
+            <td>world</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
